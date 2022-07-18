0ba90046d15509cc544f0fec6ac1b2d9a2370b9a
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-07-18 17:40:45 +0100

    new notes about import

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       
-        <Form></Form> 
+        <Form/> 
         <List/>
       
     </div>
@@ -19,5 +19,5 @@ export default App;
 /*como foi criado um componente nos não iremos repetir código, 
 vamos sempre usar esse componente para reaproveitar código
 
-
+você pode importar tanto assim: <Form/> quanto assim: <Form><Form/>
 */
