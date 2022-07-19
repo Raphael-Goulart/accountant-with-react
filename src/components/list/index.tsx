09de74ab7f11fde46092788c9b6e5d9b566ee766
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-07-19 11:30:42 +0100

    add key

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 
 function List() {
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },{
+        tarefa:'JavaScript',
+        tempo: '01:00:00'
+    },{
+        tarefa:'TypeScript',
+        tempo: '03:00:00'
+    }]
     return (
         <aside>
             <h2>Estudos do Dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        JavaScript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.tarefa} </h3>
+                        <span> {item.tempo} </span>
+                    </li>
+                ))}
+                
             </ul>
         </aside>
     )
@@ -28,4 +29,13 @@ function List() {
 
 export default List;
 
-/*está é uma estrutura padrão de function components */
+/*está é uma estrutura padrão de function components
+
+Ao invés de fazer listas na "unha" podemos criar uma constante que será uma array e dentro da 
+array um objeto ou seja com
+vários itens, no caso fica const tarefas = [{}]
+O map sempre percorre os itens de um array e retorna 
+Sempre que temos um map temos que ter uma key
+
+
+*/
