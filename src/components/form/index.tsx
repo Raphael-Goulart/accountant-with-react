91805f2e6c9059aec4751daa45be603cb28ee598
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-07-27 10:33:21 +0100

    add uuid

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ITarefa from "../../types/tarefas";
 import Button from  '../button/index';
-import style from './Form.module.scss'
+import style from './Form.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 class Form extends React.Component <{
     setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
@@ -15,7 +16,16 @@ class Form extends React.Component <{
 
     addTarefa(evento: React.FormEvent<HTMLFormElement>){
         evento.preventDefault();
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+        this.props.setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas, 
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                
+                }])
         this.setState({
             tarefa: "",
             tempo: "00:00"
